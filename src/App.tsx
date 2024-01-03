9a413e626b27e6c0f12a3b248ce6bb093ccb757f
CNine <cnine229@gmail.com>
2024-01-03 18:12:21 +0800

    chore: setup navigation

--- src/App.tsx
+++ src/App.tsx
@@ -6,49 +6,131 @@
  */
 
 import React from 'react';
+import {StyleSheet, View, Text, Button} from 'react-native';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  useColorScheme,
-  View,
-} from 'react-native';
-
+  NavigationContainer,
+  RouteProp,
+  useNavigation,
+  useRoute,
+} from '@react-navigation/native';
 import {
-  Colors,
-  Header,
-  LearnMoreLinks,
-} from 'react-native/Libraries/NewAppScreen';
+  createNativeStackNavigator,
+  NativeStackNavigationProp,
+} from '@react-navigation/native-stack';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 
-import {NavigationContainer} from '@react-navigation/native';
+const PRIMARY_COLOR = '#f4511e';
+const TITLE_COLOR = '#fff';
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+type RootParamList = {
+  Home: undefined;
+  Details: {
+    detailId: string;
   };
+};
+
+function MainScreen() {
+  const navigation =
+    useNavigation<NativeStackNavigationProp<RootParamList, 'Home'>>();
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details 123"
+        onPress={() => {
+          navigation.push('Details', {
+            detailId: '123',
+          });
+        }}
       />
-      <NavigationContainer>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={backgroundStyle}>
-          <Header />
-          <View
-            style={{
-              backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            }}>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </NavigationContainer>
-    </SafeAreaView>
+      <Button
+        title="Go to Details 456"
+        onPress={() => {
+          navigation.push('Details', {
+            detailId: '456',
+          });
+        }}
+      />
+    </View>
+  );
+}
+
+function MessageScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Feed Screen</Text>
+    </View>
+  );
+}
+
+function DetailsScreen() {
+  const route = useRoute<RouteProp<RootParamList, 'Details'>>();
+  const navigation =
+    useNavigation<NativeStackNavigationProp<RootParamList, 'Details'>>();
+  const detailId = route.params.detailId;
+
+  return (
+    <View style={styles.container}>
+      <Text>Details Screen</Text>
+      <Text>Detail ID: {detailId}</Text>
+
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+const Tab = createMaterialTopTabNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: PRIMARY_COLOR,
+          },
+          headerTintColor: TITLE_COLOR,
+          statusBarColor: PRIMARY_COLOR,
+        }}>
+        <Stack.Screen name="Home">
+          {() => (
+            <Tab.Navigator
+              initialRouteName="Main"
+              screenOptions={{
+                tabBarActiveTintColor: PRIMARY_COLOR,
+                tabBarIndicatorStyle: {
+                  backgroundColor: PRIMARY_COLOR,
+                },
+              }}>
+              <Tab.Screen name="Main" component={MainScreen} />
+              <Tab.Screen name="Message" component={MessageScreen} />
+            </Tab.Navigator>
+          )}
+        </Stack.Screen>
+        <Stack.Screen
+          name="Details"
+          component={DetailsScreen}
+          options={{title: 'Overview'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
