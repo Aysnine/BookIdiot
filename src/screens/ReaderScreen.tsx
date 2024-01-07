5b19637a1d8ef8c5b9f459ec1b874b2ee3e37f6f
CNine <cnine229@gmail.com>
2024-01-07 11:40:07 +0800

    chore: setup simple web-view demo

--- src/screens/ReaderScreen.tsx
+++ src/screens/ReaderScreen.tsx
@@ -1,34 +1,41 @@
-import React from 'react';
-import {StyleSheet, View, Text, Button} from 'react-native';
+import React, {useEffect, useRef} from 'react';
+import {StyleSheet, View} from 'react-native';
 import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {RootParamList} from '../App';
+import WebView from 'react-native-webview';
 
 const readerScreenStyles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'blue',
   },
+  wholeScreen: {flex: 1, backgroundColor: '#fff'},
 });
 
 export function ReaderScreen() {
   const route = useRoute<RouteProp<RootParamList, 'Details'>>();
+  const bookName = route.params.documentExploreMeta.pickerResult.name;
   const navigation =
     useNavigation<NativeStackNavigationProp<RootParamList, 'Details'>>();
-  const detailId = route.params.documentExploreMeta.md5;
+
+  const webview = useRef<WebView>(null);
+
+  useEffect(() => {
+    if (bookName) {
+      navigation.setOptions({
+        title: bookName,
+      });
+    }
+  }, [bookName, navigation]);
 
   return (
     <View style={readerScreenStyles.container}>
-      <Text>Details Screen</Text>
-      <Text>Detail ID: {detailId}</Text>
-
-      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
-
-      <Button
-        title="Go back to first screen in stack"
-        onPress={() => navigation.popToTop()}
+      <WebView
+        ref={webview}
+        style={[readerScreenStyles.wholeScreen]}
+        originWhitelist={['*']}
+        source={{html: '<h1>This is a static HTML source!</h1>'}}
       />
     </View>
   );
